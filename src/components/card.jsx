ffa10f3622cf81a92e3cfbcb4436fa66af9d9319
suraj a <surajra0628@gmail.com>
2023-09-09 07:58:49 +0530

    modified

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,7 @@
 export function Card(props){
     return(
         <div class="bg-white mx-5 p-2 flex flex-col items-center" >
-            <h2>{props.title}</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor unde rem eius, architecto alias nesciunt!</p>
-            <button class="btn">click me!</button>
+            {props.children}
         </div>
     )
 }
